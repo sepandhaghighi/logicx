121fd6c71f27d5be25c08775373447534de55dba
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-08-14 20:46:53 +0430

    setup file added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'logicx',
+  packages = ['logicx'],
+  version = '0.1',
+  description = 'VLSI Tool',
+  long_description="VLSI Tool",
+  author = 'Sepand Haghighi & Mohammadreza Rezaie',
+  author_email = 'sepand@qpage.ir',
+  url = 'https://github.com/sepandhaghighi/logicx',
+  download_url = 'https://github.com/sepandhaghighi/logicx/tarball/v0.1',
+  keywords = ['logic', 'VLSI', 'python3','python','digital',"generator","moduland"],
+  classifiers = [],
+  license='MIT',
+)
